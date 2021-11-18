a1dd4766e25f191584038c69399618f7097ecb80
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2021-11-18 12:18:38 -0700

    starter upload
    starter upload

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fbbe4576e902924e89b7b395fa68de1c'),
+    },
+  },
+});
